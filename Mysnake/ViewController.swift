2293330bbfced668c8ec0d1267799ea5a582d9e1
20161104612 <643024265@qq.com>
2018-12-21 16:07:33 +0800

    Initial Commit

---
+++ Mysnake/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mysnake
+//
+//  Created by 20161104612 on 2018/12/21.
+//  Copyright © 2018 20161104612. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
